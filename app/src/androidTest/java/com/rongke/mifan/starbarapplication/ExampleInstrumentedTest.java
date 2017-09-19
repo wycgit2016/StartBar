b50efde147c3e58d4ccad51be2ba49092cfb2947
wycgit2016 <1432248992@qq.com>
2017-09-19 23:42:56 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/rongke/mifan/starbarapplication/ExampleInstrumentedTest.java
@@ -0,0 +1,25 @@
+package com.rongke.mifan.starbarapplication;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * wyc创建 on 2017/6/16.
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.rongke.mifan.starbarapplication", appContext.getPackageName());
+    }
+}
---
+++ app/src/test/java/com/rongke/mifan/starbarapplication/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package com.rongke.mifan.starbarapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * wyc创建 on 2017/6/16.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
